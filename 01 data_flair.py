d6d24e0a4548a3d1dab9727efd3ef730dcfaec80
Michael Jamie <kramstyles@outlook.com>
2021-09-20 23:05:06 +0100

    got it to work at first

--- 01 data_flair.py
+++ 01 data_flair.py
@@ -8,4 +8,8 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 
 from os import path
 
-data = pd.read_csv(path.join())
+# data = pd.read_csv(path.join('assets/files', 'news1000.csv'))
+# data = pd.read_csv(path.join('assets/files', 'news.csv'))
+data = pd.read_excel(path.join('assets/files', 'news1000.xlsx'))
+print(data.shape) # Number of rows and columns
+print(data.head())
